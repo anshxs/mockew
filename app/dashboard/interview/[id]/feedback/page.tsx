9afa6111627f9ff415326c9ea33ee3088bc1edb9
Ansh Sharma <anshsxa@gmail.com>
2025-05-06 18:37:28 +0530

    Update page.tsx

--- app/dashboard/interview/[id]/feedback/page.tsx
+++ app/dashboard/interview/[id]/feedback/page.tsx
@@ -3,13 +3,12 @@ import { getInterviewById } from "@/lib/actions/general.action";
 import { redirect } from "next/navigation";
 import FeedbackClient from "./FeedbackClient";
 
-const Feedback = async ({ params }: RouteParams) => {
-    const { id } = params;
-  
-    const interview = await getInterviewById(id);
-    if (!interview) redirect("/dashboard");
-  
-    return <FeedbackClient interviewId={id} interview={interview} />;
-  }
+const Feedback = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const interview = await getInterviewById(id);
+  if (!interview) redirect("/dashboard");
+
+  return <FeedbackClient interviewId={id} interview={interview} />;
+};
   
   export default Feedback;
