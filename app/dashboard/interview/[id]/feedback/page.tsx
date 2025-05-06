87a5eed16fd5ebb5b8e0c3970ba2659d7581d1c2
Ansh Sharma <anshsxa@gmail.com>
2025-05-06 19:06:16 +0530

    Update page.tsx

--- app/dashboard/interview/[id]/feedback/page.tsx
+++ app/dashboard/interview/[id]/feedback/page.tsx
@@ -1,18 +1,25 @@
-// app/dashboard/interview/[id]/feedback/page.tsx
+// app/dashboard/[id]/feedback/page.tsx
 
 import { getInterviewById } from "@/lib/actions/general.action";
 import { redirect } from "next/navigation";
 import FeedbackClient from "./FeedbackClient";
 
-export default async function Feedback({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
+type FeedbackPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const FeedbackPage = async ({ params }: FeedbackPageProps) => {
+  const id = params.id;
 
   const interview = await getInterviewById(id);
-  if (!interview) redirect("/dashboard");
+
+  if (!interview) {
+    redirect("/dashboard");
+  }
 
   return <FeedbackClient interviewId={id} interview={interview} />;
-}
+};
+
+export default FeedbackPage;
