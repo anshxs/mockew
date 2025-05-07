19b20d5b64bf295c46d6116b3da70209c10860f0
Ansh Sharma <anshsxa@gmail.com>
2025-05-07 07:53:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
   return (
     
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 md:px-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen px-8 py-8 pb-20 max-w-screen gap-16 md:px-20 font-[family-name:var(--font-geist-sans)]">
       <BlurFade>
       <main className="flex flex-col gap-[32px] items-center justify-center">
       
